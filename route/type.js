8dff0dc8ef259cecd36ce689d6dbbef2263fd63e
Blandine <blandine.mallart@gmail.com>
2020-04-16 21:32:55 +0200

    feat(tripperCountries) route of num tripper who saw countries

--- route/type.js
+++ route/type.js
@@ -97,4 +97,25 @@ router.get("/countries", (req, res) => {
     }
   );
 });
+
+// Route who saw
+router.get("/countries/tripper", (req, res) => {
+
+  // Connection to the database and selection of information
+  connection.query(
+    `SELECT pays.id AS id_pays, pays.name, count(*) as numOfVisited, pays.nameFr, pays.flag, pays.pictures
+      FROM assoc_pays_periodes_users
+        INNER JOIN pays on pays.id=assoc_pays_periodes_users.id_pays 
+        INNER JOIN periodes on periodes.id=assoc_pays_periodes_users.id_periodes
+      GROUP BY pays.id;`,
+    (err, results) => {
+      if (err) {
+        // If an error has occurred, then the user is informed of the error
+        res.status(500).send("Error in route for see the tripper");
+      }
+      res.json(results);
+    }
+  );
+});
+
 module.exports = router;
